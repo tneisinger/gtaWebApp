47a1b1767f455f56b8042af0379593f423104ec7
tjneisi <tjneisi@gmail.com>
2018-02-28 15:41:35 -0800

    Add code coverage to flask

--- services/flask/manage.py
+++ services/flask/manage.py
@@ -1,16 +1,21 @@
 # services/flask/manage.py
 
-
-
 import unittest
-
-
+import coverage
 from flask.cli import FlaskGroup
-
 
 from project import create_app, db
 from project.admin.models import User
 
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -38,6 +43,20 @@ def seed_db():
     db.session.add(User(username='michaelherman', email="michael@mherman.org"))
     db.session.commit()
 
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
