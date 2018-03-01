c5d38eb02cb01e8087727fe46eac51e440c541e6
tjneisi <tjneisi@gmail.com>
2018-02-28 18:39:26 -0800

    Add react

---
+++ services/client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <br/>
+          <h1>All Users</h1>
+          <hr/><br/>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
