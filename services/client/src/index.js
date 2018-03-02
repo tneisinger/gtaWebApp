e301e61049dd0c72309f37379204d5d5a515cf94
tjneisi <tjneisi@gmail.com>
2018-03-01 19:52:55 -0800

    Add enzyme for react testing

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import UsersList from './components/UsersList';
 
-const App = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <br/>
-          <h1>All Users</h1>
-          <hr/><br/>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  };
+
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/admin/users`)
+    .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => { console.log(err); });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <br/>
+            <h1>All Users</h1>
+            <hr/><br/>
+            <UsersList users={this.state.users}/>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 };
 
 ReactDOM.render(
