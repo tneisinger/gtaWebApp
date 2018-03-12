bbd8461aaf05614a34a31a61bed7f63c1ec41eec
tjneisi <tjneisi@gmail.com>
2018-03-12 13:15:58 -0700

    Add route and tests for getting user status

--- services/flask/project/admin/api.py
+++ services/flask/project/admin/api.py
@@ -467,3 +467,25 @@ def logout_user():
             return jsonify(response_object), 401
     else:
         return jsonify(response_object), 403
+
+@admin_blueprint.route('/auth/status', methods=['GET'])
+def get_user_status():
+    # get auth token
+    auth_header = request.headers.get('Authorization')
+    response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.'
+    }
+    if auth_header:
+        auth_token = auth_header.split(' ')[1]
+        resp = User.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            user = User.query.filter_by(id=resp).first()
+            response_object['status'] = 'success'
+            response_object['message'] = 'Success.'
+            response_object['data'] = user.to_json()
+            return jsonify(response_object), 200
+        response_object['message'] = resp
+        return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 401
--- services/flask/project/tests/test_admin_auth.py
+++ services/flask/project/tests/test_admin_auth.py
@@ -214,6 +214,41 @@ class TestAuthBlueprint(BaseTestCase):
                 data['message'] == 'Invalid token. Please log in again.')
             self.assertEqual(response.status_code, 401)
 
+    def test_user_status(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            resp_login = self.client.post(
+                '/admin/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['auth_token']
+            response = self.client.get(
+                '/admin/auth/status',
+                headers={'Authorization': f'Bearer {token}'}
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['data'] is not None)
+            self.assertTrue(data['data']['username'] == 'test')
+            self.assertTrue(data['data']['email'] == 'test@test.com')
+            self.assertFalse(data['data']['is_admin'])
+            self.assertEqual(response.status_code, 200)
+
+    def test_invalid_status(self):
+        with self.client:
+            response = self.client.get(
+                '/admin/auth/status',
+                headers={'Authorization': 'Bearer invalid'})
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(
+                data['message'] == 'Invalid token. Please log in again.')
+            self.assertEqual(response.status_code, 401)
+
 
 if __name__ == '__main__':
     unittest.main()
