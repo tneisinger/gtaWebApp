7a73499e969aa5296ef3f476c6935781ee2bb2b6
tjneisi <tjneisi@gmail.com>
2018-03-13 17:04:04 -0700

    Add tests for /admin/calendar and make them pass

--- services/flask/project/admin/decorators.py
+++ services/flask/project/admin/decorators.py
@@ -18,24 +18,32 @@ class check_user_is_admin(object):
 
         @wraps(route_function)
         def wrapper():
-            auth_header = request.headers.get('Authorization')
-            if auth_header:
-                auth_token = auth_header.split(' ')[1]
-                resp = User.decode_auth_token(auth_token)
-                user = User.query.filter_by(id=resp).first()
-                if user.is_admin:
-
-                    # user is_admin. Let the user use the route
-                    if self.pass_user:
-                        return route_function(user)
-                    else:
-                        return route_function()
-
-            # If the user is not signed in or isn't admin, deny access
             response_object = {
                 'status': 'fail',
                 'message': 'Provide a valid auth token.'
             }
-            return jsonify(response_object), 400
+            auth_header = request.headers.get('Authorization')
+            if auth_header:
+                auth_token = auth_header.split(' ')[1]
+                resp = User.decode_auth_token(auth_token)
+
+                # If the token is invalid, deny access
+                if not isinstance(resp, int):
+                    return jsonify(response_object), 401
+
+                # If the token is valid and the user is_admin, allow access
+                user = User.query.filter_by(id=resp).first()
+                if user.is_admin:
+                    if self.pass_user:
+                        return route_function(user)
+                    else:
+                        return route_function()
+                else:
+                    # Tell the user that he/she must be admin
+                    response_object['message'] = 'User must be admin.'
+                    return jsonify(response_object), 401
+
+            # If the user is not signed in or isn't admin, deny access
+            return jsonify(response_object), 401
 
         return wrapper
--- services/flask/project/tests/test_admin_calendar.py
+++ services/flask/project/tests/test_admin_calendar.py
@@ -49,6 +49,10 @@ class TestAdminCalendarRoutes(BaseTestCase):
         'category': next(e.value for e in OneTimeExpense.Category),
     }
 
+    FAKE_TOKEN = ('wnK6eXAiOiJK21QiLfJhbqciOiJIUzI1NiJ9.eyJleHAiOj' +
+                  'E1MjA5ODMyNzYsImlhdCIDMTUyMDk4MzI3Mywic3ViIjoxfQ.k' +
+                  'YQbY1Fj8pB4oK2nHmRuPWp3bDtHFwa000000000000')
+
     def test_get_calendar_events(self):
         """
         Test getting jobs and one-time-expenses that fall between the dates
@@ -124,6 +128,168 @@ class TestAdminCalendarRoutes(BaseTestCase):
             self.assertEqual(data['data']['user']['email'],
                              self.VALID_USER_DICT1['email'])
             self.assertEqual(data['data']['user']['id'], 1)
+
+    def test_get_calendar_events_not_signed_in(self):
+        """
+        Test attempting to get jobs and one-time-expenses (calendar events)
+        without being signed in.
+        """
+        # Add a user to the db
+        user = add_user(**self.VALID_USER_DICT1)
+
+        # Set the user's is_admin database column to True
+        make_user_admin(user)
+
+        # Add a job and an expense to the database
+        add_job(**self.VALID_JOB_DICT1)
+        add_one_time_expense(**self.VALID_EXPENSE_DICT1)
+
+        with self.client:
+
+            # request the calendar data
+            request_data = {
+                     'start_date': self.FIRST_DAY.isoformat(),
+                     'end_date': self.LAST_DAY.isoformat()
+            }
+            response = self.client.get(
+                url_for('admin.get_calendar_events', **request_data)
+            )
+            data = json.loads(response.data.decode())
+
+            # Assert that it failed
+            self.assertEqual(response.status_code, 401)
+            self.assertEqual(data['status'], 'fail')
+            self.assertEqual(data['message'], 'Provide a valid auth token.')
+
+    def test_get_calendar_events_fake_auth_token(self):
+        """
+        Test attempting to get calendar events using a fake auth token
+        """
+        # Add a user to the db
+        user = add_user(**self.VALID_USER_DICT1)
+
+        # Set the user's is_admin database column to True
+        make_user_admin(user)
+
+        # Add a job and an expense to the database
+        add_job(**self.VALID_JOB_DICT1)
+        add_one_time_expense(**self.VALID_EXPENSE_DICT1)
+
+        with self.client:
+
+            # login as user
+            self.client.post(
+                '/admin/auth/login',
+                data=json.dumps({
+                    'email': self.VALID_USER_DICT1['email'],
+                    'password': self.VALID_USER_DICT1['password']
+                }),
+                content_type='application/json'
+            )
+
+            # request the calendar data
+            request_data = {
+                     'start_date': self.FIRST_DAY.isoformat(),
+                     'end_date': self.LAST_DAY.isoformat()
+            }
+            response = self.client.get(
+                url_for('admin.get_calendar_events', **request_data),
+                headers={'Authorization': f'Bearer {self.FAKE_TOKEN}'}
+            )
+            data = json.loads(response.data.decode())
+
+            # Assert that it failed
+            self.assertEqual(response.status_code, 401)
+            self.assertEqual(data['status'], 'fail')
+            self.assertEqual(data['message'], 'Provide a valid auth token.')
+
+    def test_get_calendar_events_empty_auth_header(self):
+        """
+        Test attempting to get calendar events with an empty Authorization
+        header
+        """
+        # Add a user to the db
+        user = add_user(**self.VALID_USER_DICT1)
+
+        # Set the user's is_admin database column to True
+        make_user_admin(user)
+
+        # Add a job and an expense to the database
+        add_job(**self.VALID_JOB_DICT1)
+        add_one_time_expense(**self.VALID_EXPENSE_DICT1)
+
+        with self.client:
+
+            # login as user
+            self.client.post(
+                '/admin/auth/login',
+                data=json.dumps({
+                    'email': self.VALID_USER_DICT1['email'],
+                    'password': self.VALID_USER_DICT1['password']
+                }),
+                content_type='application/json'
+            )
+
+            # request the calendar data
+            request_data = {
+                     'start_date': self.FIRST_DAY.isoformat(),
+                     'end_date': self.LAST_DAY.isoformat()
+            }
+            response = self.client.get(
+                url_for('admin.get_calendar_events', **request_data),
+                headers={'Authorization': ''}
+            )
+            data = json.loads(response.data.decode())
+
+            # Assert that it failed
+            self.assertEqual(response.status_code, 401)
+            self.assertEqual(data['status'], 'fail')
+            self.assertEqual(data['message'], 'Provide a valid auth token.')
+
+    def test_get_calendar_events_user_not_admin(self):
+        """
+        Test attempting to get calendar events with an empty Authorization
+        header
+        """
+        # Add a user to the db
+        add_user(**self.VALID_USER_DICT1)
+
+        # Leave the user's is_admin database column set to False
+
+        # Add a job and an expense to the database
+        add_job(**self.VALID_JOB_DICT1)
+        add_one_time_expense(**self.VALID_EXPENSE_DICT1)
+
+        with self.client:
+
+            # login as user
+            resp_login = self.client.post(
+                '/admin/auth/login',
+                data=json.dumps({
+                    'email': self.VALID_USER_DICT1['email'],
+                    'password': self.VALID_USER_DICT1['password']
+                }),
+                content_type='application/json'
+            )
+
+            # get the user's auth token
+            token = json.loads(resp_login.data.decode())['auth_token']
+
+            # request the calendar data
+            request_data = {
+                     'start_date': self.FIRST_DAY.isoformat(),
+                     'end_date': self.LAST_DAY.isoformat()
+            }
+            response = self.client.get(
+                url_for('admin.get_calendar_events', **request_data),
+                headers={'Authorization': f'Bearer {token}'}
+            )
+            data = json.loads(response.data.decode())
+
+            # Assert that it failed
+            self.assertEqual(response.status_code, 401)
+            self.assertEqual(data['status'], 'fail')
+            self.assertEqual(data['message'], 'User must be admin.')
 
 
 if __name__ == "__main__":
