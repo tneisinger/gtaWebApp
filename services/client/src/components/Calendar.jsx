86f8d57a579c6b7a6adc13054963b801ac57a0dd
tjneisi <tjneisi@gmail.com>
2018-03-17 12:12:49 -0700

    Remove view buttons from calendar toolbar

--- services/client/src/components/Calendar.jsx
+++ services/client/src/components/Calendar.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import BigCalendar from 'react-big-calendar'
+import React from 'react';
+import BigCalendar, { BackIcon, NextIcon } from 'react-big-calendar';
+import Toolbar from 'react-big-calendar';
 import dates from 'react-big-calendar/lib/utils/dates';
-import moment from 'moment'
-import '../../node_modules/react-big-calendar/lib/css/react-big-calendar.css'
-import '../css/calendar.css'
+import moment from 'moment';
+import '../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
+import '../css/calendar.css';
 
 // select moment as the localizer for BigCalendar
 BigCalendar.momentLocalizer(moment);
+
+
+const CustomToolbar = (toolbar) => {
+  const goToBack = () => {
+    toolbar.date.setMonth(toolbar.date.getMonth() - 1);
+    toolbar.onNavigate('prev');
+  };
+
+  const goToNext = () => {
+    toolbar.date.setMonth(toolbar.date.getMonth() + 1);
+    toolbar.onNavigate('next');
+  };
+
+  const goToCurrent = () => {
+    const now = new Date();
+    toolbar.date.setMonth(now.getMonth());
+    toolbar.date.setYear(now.getFullYear());
+    toolbar.onNavigate('current');
+  };
+
+  const label = () => {
+    const date = moment(toolbar.date);
+    return (
+      <span>
+        <b>{date.format('MMMM')}</b>
+        <span> {date.format('YYYY')}</span>
+      </span>
+    );
+  };
+
+  //return (
+    //<div className='toolbar-container'>
+      //<label className='label-date'>{label()}</label>
+
+      //<div className='back-next-buttons'>
+        //<button className='btn-back' onClick={goToBack}>&#8249;</button>
+        //<button className='btn-current' onClick={goToCurrent}>
+          //today
+        //</button>
+        //<button className='btn-next' onClick={goToNext}>&#8250;</button>
+      //</div>
+    //</div >
+  //);
+
+
+  return (
+    <div className='rbc-toolbar'>
+      <span className='rbc-btn-group'>
+        <button type='button' onClick={goToBack}>prev</button>
+        <button type='button' onClick={goToNext}>next</button>
+      </span>
+      <span className='rbc-toolbar-label'>{label()}</span>
+      <span className='rbc-btn-group'>
+        <button type='button' onClick={goToCurrent}>today</button>
+      </span>
+    </div >
+  );
+}
 
 class Calendar extends React.Component {
 
@@ -25,7 +84,6 @@ class Calendar extends React.Component {
   componentDidMount() {
     this.getDateRange();
   };
-
 
   bindScopes(keys) {
     for (let key of keys) {
@@ -54,6 +112,9 @@ class Calendar extends React.Component {
           events={this.state.events}
           startAccessor={'start'}
           endAccessor={'end'}
+          components={{
+            toolbar: CustomToolbar
+          }}
         />
       </div>
     );
