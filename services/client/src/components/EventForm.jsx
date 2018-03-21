8de96a67e32914947e4b83843a5a428d6f22aec1
tjneisi <tjneisi@gmail.com>
2018-03-21 11:24:56 -0700

    Remove if statements from EventForm.jsx
    Prior to this commit, there were multiple opening <form> tags
    in EventForm.jsx.  For the sake of DRYness, there is now only
    one <form> tag.  To accomplish this, I had to remove the if
    statements and replace them with react conditional rendering.  The
    behavior is the same, but I had to wrap the form-group divs inside
    an outer div so that there would only be one top-level element in
    the conditionally rendered blocks.

--- services/client/src/components/EventForm.jsx
+++ services/client/src/components/EventForm.jsx
@@ -32,246 +32,246 @@ formData[formTypes[1]] = emptyOneTimeExpenseFormData;
 
 
 const EventForm = (props) => {
-  if (props.formType === formTypes[0]) {
-    const data = props.formData[formTypes[0]];
-    return (
-      <form onSubmit={(event) => props.handleFormSubmit(event)}>
+  const data = props.formData[props.formType];
+  return (
+    <form onSubmit={(event) => props.handleFormSubmit(event)}>
+      {props.formType === formTypes[0] &&
 
-        <div className="form-group">
-          <input
-            name="client"
-            className="form-control input-lg"
-            type="text"
-            placeholder="Client Name"
-            required
-            value={data.client}
-            onChange={props.handleFormChange}
-          />
+        <div>
+          <div className="form-group">
+            <input
+              name="client"
+              className="form-control input-lg"
+              type="text"
+              placeholder="Client Name"
+              required
+              value={data.client}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              name="description"
+              className="form-control input-lg"
+              type="text"
+              placeholder="Description"
+              required
+              value={data.description}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              name="amount_paid"
+              className="form-control input-lg"
+              type="number"
+              placeholder="Amount Paid"
+              required
+              value={data.amount_paid}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="paid_to">Paid To</label>
+            <select
+              id="paid_to"
+              name="paid_to"
+              className="form-control input-lg"
+              placeholder="Paid To"
+              required
+              value={data.paid_to}
+              onChange={props.handleFormChange}
+            >
+              <option value="Gladtime Audio">Gladtime Audio</option>
+              <option value="Meghan">Meghan</option>
+              <option value="Tyler">Tyler</option>
+              <option value="Tyler/Meghan Separately">
+                Tyler/Meghan Separately
+              </option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="worked_by">Worked By</label>
+            <select
+              id="worked_by"
+              name="worked_by"
+              className="form-control input-lg"
+              required
+              value={data.worked_by}
+              onChange={props.handleFormChange}
+            >
+              <option value="Meghan">Meghan</option>
+              <option value="Tyler">Tyler</option>
+              <option value="Tyler and Meghan">Tyler and Meghan</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="confirmation">Confirmation</label>
+            <select
+              id="confirmation"
+              name="confirmation"
+              className="form-control input-lg"
+              required
+              value={data.confirmation}
+              onChange={props.handleFormChange}
+            >
+              <option value="Confirmed">Confirmed</option>
+              <option value="Pencilled In">Pencilled In</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="has_paid">Has Paid</label>
+            <input
+              id="has_paid"
+              name="has_paid"
+              className="form-check-input checkbox-lg"
+              type="checkbox"
+              value={data.has_paid}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="start_date">Start Date</label>
+            <input
+              id="start_date"
+              name="start_date"
+              className="form-control input-lg"
+              type="date"
+              required
+              value={data.start_date}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="end_date">End Date</label>
+            <input
+              id="end_date"
+              name="end_date"
+              className="form-control input-lg"
+              type="date"
+              required
+              value={data.end_date}
+              onChange={props.handleFormChange}
+            />
+          </div>
         </div>
+      }
 
-        <div className="form-group">
-          <input
-            name="description"
-            className="form-control input-lg"
-            type="text"
-            placeholder="Description"
-            required
-            value={data.description}
-            onChange={props.handleFormChange}
-          />
+      {/* If the formType is OneTimeExpense... */}
+      {props.formType === formTypes[1] &&
+        <div>
+          <div className="form-group">
+            <input
+              name="merchant"
+              className="form-control input-lg"
+              type="text"
+              placeholder="Bought From"
+              required
+              value={data.merchant}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              name="description"
+              className="form-control input-lg"
+              type="text"
+              placeholder="Description"
+              required
+              value={data.description}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              name="amount_spent"
+              className="form-control input-lg"
+              type="number"
+              placeholder="Amount Spent"
+              required
+              value={data.amount_spent}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="date">Date</label>
+            <input
+              id="date"
+              name="date"
+              className="form-control input-lg"
+              type="date"
+              required
+              value={data.date}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="paid_by">Paid By</label>
+            <select
+              id="paid_by"
+              name="paid_by"
+              className="form-control input-lg"
+              required
+              value={data.paid_by}
+              onChange={props.handleFormChange}
+            >
+              <option value="Gladtime Audio">Gladtime Audio</option>
+              <option value="Meghan">Meghan</option>
+              <option value="Tyler">Tyler</option>
+              <option value="Tyler/Meghan Separately">
+                Tyler/Meghan Separately
+              </option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="tax_deductible">Tax Deductible</label>
+            <input
+              id="tax_deductible"
+              name="tax_deductible"
+              className="form-check-input checkbox-lg"
+              type="checkbox"
+              value={data.tax_deductible}
+              onChange={props.handleFormChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="category">Category</label>
+            <select
+              id="category"
+              name="category"
+              className="form-control input-lg"
+              required
+              value={data.category}
+              onChange={props.handleFormChange}
+            >
+              <option value="Business Equipment">Business Equipment</option>
+              <option value="Business Supplies">Business Supplies</option>
+              <option value="Gasoline">Gasoline</option>
+              <option value="Vehicle Maintenance">Vehicle Maintenance</option>
+              <option value="Travel Expense">Travel Expense</option>
+              <option value="Entertainment">Entertainment</option>
+              <option value="Food">Food</option>
+            </select>
+          </div>
         </div>
+      }
 
-        <div className="form-group">
-          <input
-            name="amount_paid"
-            className="form-control input-lg"
-            type="number"
-            placeholder="Amount Paid"
-            required
-            value={data.amount_paid}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="paid_to">Paid To</label>
-          <select
-            id="paid_to"
-            name="paid_to"
-            className="form-control input-lg"
-            placeholder="Paid To"
-            required
-            value={data.paid_to}
-            onChange={props.handleFormChange}
-          >
-            <option value="Gladtime Audio">Gladtime Audio</option>
-            <option value="Meghan">Meghan</option>
-            <option value="Tyler">Tyler</option>
-            <option value="Tyler/Meghan Separately">
-              Tyler/Meghan Separately
-            </option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="worked_by">Worked By</label>
-          <select
-            id="worked_by"
-            name="worked_by"
-            className="form-control input-lg"
-            required
-            value={data.worked_by}
-            onChange={props.handleFormChange}
-          >
-            <option value="Meghan">Meghan</option>
-            <option value="Tyler">Tyler</option>
-            <option value="Tyler and Meghan">Tyler and Meghan</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="confirmation">Confirmation</label>
-          <select
-            id="confirmation"
-            name="confirmation"
-            className="form-control input-lg"
-            required
-            value={data.confirmation}
-            onChange={props.handleFormChange}
-          >
-            <option value="Confirmed">Confirmed</option>
-            <option value="Pencilled In">Pencilled In</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="has_paid">Has Paid</label>
-          <input
-            id="has_paid"
-            name="has_paid"
-            className="form-check-input checkbox-lg"
-            type="checkbox"
-            value={data.has_paid}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="start_date">Start Date</label>
-          <input
-            id="start_date"
-            name="start_date"
-            className="form-control input-lg"
-            type="date"
-            required
-            value={data.start_date}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="end_date">End Date</label>
-          <input
-            id="end_date"
-            name="end_date"
-            className="form-control input-lg"
-            type="date"
-            required
-            value={data.end_date}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-      </form>
-    )
-  } else if (props.formType === formTypes[1]) {
-    const data = props.formData[formTypes[1]];
-    return (
-      <form onSubmit={(event) => props.handleFormSubmit(event)}>
-
-        <div className="form-group">
-          <input
-            name="merchant"
-            className="form-control input-lg"
-            type="text"
-            placeholder="Bought From"
-            required
-            value={data.merchant}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            name="description"
-            className="form-control input-lg"
-            type="text"
-            placeholder="Description"
-            required
-            value={data.description}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            name="amount_spent"
-            className="form-control input-lg"
-            type="number"
-            placeholder="Amount Spent"
-            required
-            value={data.amount_spent}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="date">Date</label>
-          <input
-            id="date"
-            name="date"
-            className="form-control input-lg"
-            type="date"
-            required
-            value={data.date}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="paid_by">Paid By</label>
-          <select
-            id="paid_by"
-            name="paid_by"
-            className="form-control input-lg"
-            required
-            value={data.paid_by}
-            onChange={props.handleFormChange}
-          >
-            <option value="Gladtime Audio">Gladtime Audio</option>
-            <option value="Meghan">Meghan</option>
-            <option value="Tyler">Tyler</option>
-            <option value="Tyler/Meghan Separately">
-              Tyler/Meghan Separately
-            </option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="tax_deductible">Tax Deductible</label>
-          <input
-            id="tax_deductible"
-            name="tax_deductible"
-            className="form-check-input checkbox-lg"
-            type="checkbox"
-            value={data.tax_deductible}
-            onChange={props.handleFormChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="category">Category</label>
-          <select
-            id="category"
-            name="category"
-            className="form-control input-lg"
-            required
-            value={data.category}
-            onChange={props.handleFormChange}
-          >
-            <option value="Business Equipment">Business Equipment</option>
-            <option value="Business Supplies">Business Supplies</option>
-            <option value="Gasoline">Gasoline</option>
-            <option value="Vehicle Maintenance">Vehicle Maintenance</option>
-            <option value="Travel Expense">Travel Expense</option>
-            <option value="Entertainment">Entertainment</option>
-            <option value="Food">Food</option>
-          </select>
-        </div>
-
-      </form>
-    )
-  }
+    </form>
+  )
 };
 
 EventForm.propTypes = {
