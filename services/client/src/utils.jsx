7971b8a2b8b3487536f3f7a7d0cdbfc3ed7faa54
tjneisi <tjneisi@gmail.com>
2018-03-21 21:49:39 -0700

    Add deepcopy function to utils.jsx

--- services/client/src/utils.jsx
+++ services/client/src/utils.jsx
@@ -3,4 +3,9 @@
 // Make and return a shallow copy of the input object
 export const copy = (object) => {
   return Object.assign({}, object);
-}
+};
+
+// Make and return a deep copy of the input object
+export const deepcopy = (object) => {
+  return JSON.parse(JSON.stringify(object));
+};
