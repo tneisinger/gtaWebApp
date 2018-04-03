4fb4a89d17270f7f72593ec97e8706f22927cd40
tjneisi <tjneisi@gmail.com>
2018-04-02 18:16:56 -0700

    Fix eslint errors

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter, BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App.jsx';
+import App from './App';
 
 const AppWithRouter = withRouter(App);
 
--- services/client/src/utils.jsx
+++ services/client/src/utils.jsx
@@ -1,11 +1,7 @@
 // Convenient utility functions
 
 // Make and return a shallow copy of the input object
-export const copy = (object) => {
-  return Object.assign({}, object);
-};
+export const copy = object => Object.assign({}, object);
 
 // Make and return a deep copy of the input object
-export const deepcopy = (object) => {
-  return JSON.parse(JSON.stringify(object));
-};
+export const deepcopy = object => JSON.parse(JSON.stringify(object));
