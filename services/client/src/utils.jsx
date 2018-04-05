72314612dd666a4749f36b65664c8362036ae326
tjneisi <tjneisi@gmail.com>
2018-04-04 18:39:51 -0700

    Add datestringToDate function to utils.jsx

--- services/client/src/utils.jsx
+++ services/client/src/utils.jsx
@@ -5,3 +5,11 @@ export const copy = object => Object.assign({}, object);
 
 // Make and return a deep copy of the input object
 export const deepcopy = object => JSON.parse(JSON.stringify(object));
+
+// convert a datestring of the form yyyy-mm-dd to a js Date
+export const datestringToDate = datestring => {
+  const year = parseInt(datestring.slice(0,4), 10);
+  const month = parseInt(datestring.slice(5,7), 10) - 1;
+  const day = parseInt(datestring.slice(8,10), 10);
+  return new Date(year, month, day);
+};
