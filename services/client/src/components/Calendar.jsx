294164b99c5a4c577245532e3fc8a55f873c82ec
tjneisi <tjneisi@gmail.com>
2018-04-12 20:23:23 -0700

    Improve, rename createBigCalendarEvents function

--- services/client/src/components/Calendar.jsx
+++ services/client/src/components/Calendar.jsx
@@ -44,7 +44,7 @@ class Calendar extends Component {
         Authorization: `Bearer ${window.localStorage.getItem('authToken')}`,
       },
     }).then((res) => {
-      const bcEvents = convertEventsToBCEvents(res.data.data);
+      const bcEvents = createBigCalendarEvents(res.data.data);
       this.props.setCalendarEvents(bcEvents);
     }).catch((err) => {
       // TODO: [err.response.data, err.response.status];
@@ -88,30 +88,41 @@ class Calendar extends Component {
 
 
 // Convert an object of jobs and oneTimeExpenses (as they are received from the
-// '/admin/events' api route) into a list of js objects that are in a form that
-// is compatible with React Big Calendar
-function convertEventsToBCEvents(events) {
+// '/admin/events' api route) into a list of React Big Calendar events.
+function createBigCalendarEvents(events) {
   let result = [];
+
+  // Convert each job into a React Big Calendar event
   events.jobs.forEach(job => {
     let bcEvent = {
+      eventType: 'job',
       id: job.id,
       title: job.client,
       start: datestringToDate(job.start_date),
       end: datestringToDate(job.end_date),
       allDay: true,
     }
+
+    // Add all the attributes from the job to the bcEvent
+    for (var attrname in job) { bcEvent[attrname] = job[attrname]; }
     result.push(bcEvent);
   });
+
+  // Convert each oneTimeExpense into a React Big Calendar event
   events.expenses.forEach(expense => {
     let bcEvent = {
+      eventType: 'oneTimeExpense',
       id: expense.id,
       title: expense.merchant,
       start: datestringToDate(expense.date),
       end: datestringToDate(expense.date),
       allDay: true,
     }
+    // Add all the attributes from the job to the bcEvent
+    for (var attrname in expense) { bcEvent[attrname] = expense[attrname]; }
     result.push(bcEvent);
   });
+
   return result;
 }
 
