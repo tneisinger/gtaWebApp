f9f5590b0840c1cd7e195054da8fe8c0592a9bf2
tjneisi <tjneisi@gmail.com>
2018-07-10 19:49:22 -0700

    Add clickCalendarDays function to testUtils
    The clickCalendarDays function is an abstraction of the process
    required to click on a date range of the Calendar component.  I
    decided to break this procedure out into its own function in order
    to help simplify the code in App.test.jsx.

--- services/client/src/testUtils.jsx
+++ services/client/src/testUtils.jsx
@@ -22,3 +22,31 @@ export class LocalStorageMock {
     delete this.store[key];
   }
 }
+
+// A function that simulates a click on a day-range of the React Big Calendar
+// dayStart and dayEnd are integers that represent the day of the month.
+// dayStart should always be <= dayEnd.  This function will not check
+// that condition.
+export function clickCalendarDays(app, calendar, dayStart, dayEnd) {
+  // create the startDate date object
+  let today = new Date();
+  const thisYear = today.getFullYear();
+  const thisMonth = today.getMonth();
+  const startDate = new Date(thisYear, thisMonth, dayStart);
+
+  // if dayEnd was provided, use it to create the endDate date object.
+  // Otherwise, just use the startDate as the endDate
+  const endDate = dayEnd ? new Date(thisYear, thisMonth, dayEnd) : startDate;
+
+  const clickedDates = {
+    start: startDate,
+    end: endDate,
+  };
+
+  // Fake a calendar click by directly running Calendar's onSelectSlot method.
+  app.find(calendar).prop('onSelectSlot')(clickedDates);
+
+  app.update();
+
+  return clickedDates;
+}
